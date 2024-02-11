7082d20f8c85cf743ed4c278a83ccbcc06f26fe7
Ubuntu <ubuntu@ip-172-31-21-140.eu-north-1.compute.internal>
2024-02-11 16:43:23 +0000

    production ready

--- sifirtas/settings/production.py
+++ sifirtas/settings/production.py
@@ -1,4 +1,11 @@
-from base import *
-from djoser_settings import *
-STATIC_URL = "static/"
+import os
+from .base import *
+from .djoser_settings import *
 DEBUG = False
+ALLOWED_HOSTS = ['13.50.180.137']
+MEDIA_URL = 'media/'
+STATIC_URL = 'static/'
+STATIC_ROOT = 'staticfiles/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/' # Here
+    ]

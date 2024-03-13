22c59444b507237e80f5b7eee814d9d272bd8785
ali fareeq <alifareeq77@gmail.com>
2024-03-13 22:19:07 +0300

    merged new update for new models

--- sifirtas/settings/production.py
+++ sifirtas/settings/production.py
@@ -1,11 +1,10 @@
 import os
 from .base import *
 from .djoser_settings import *
-DEBUG = False
-ALLOWED_HOSTS = ['13.50.180.137']
+
+DEBUG = True
+ALLOWED_HOSTS = ['13.50.180.137', '127.0.0.1', 'localhost']
 MEDIA_URL = 'media/'
-STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static/' # Here
-    ]
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
